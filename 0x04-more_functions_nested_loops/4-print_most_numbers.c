263a0f4ff7b9f1bb0aa085d6091e8a9fa5f5ab1f
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-09-15 04:30:28 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers 0 to 9 excluding 2 and 4
+ *
+ */
+
+void print_most_numbers(void)
+{
+	char i = '0';
+
+	while (i <= 9)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+}
