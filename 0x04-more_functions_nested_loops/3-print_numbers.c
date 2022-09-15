1fb144838880f4dea81887b568471ece92c3b323
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-09-15 04:20:07 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		_putchar(i);
+
+		i++;
+	}
+
+	_putchar('\n');
+}
