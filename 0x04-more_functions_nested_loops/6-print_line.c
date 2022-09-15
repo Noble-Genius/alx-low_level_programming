97bf3a7493b63ee6bb99b1b92f0da5c4c0c56340
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-09-15 13:15:46 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ *
+ * @n: type integer
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+}
