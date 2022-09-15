b6719a930ddac49c606bd00cc48c1489c6bce2a5
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-09-15 03:45:32 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integer inputs
+ *
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+	return (result);
+}
