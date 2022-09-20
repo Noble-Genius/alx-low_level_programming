172a118081543284c8a80b421a3c065bbf385781
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-09-20 09:45:24 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates value of a variable outside the function
+ * using a pointer
+ *
+ * @n : pointer of type int
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
