f3bfe3fe55ba1e4bcd1513c47aa25b0c7aa1485b
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-09-20 11:04:34 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns length of a string
+ *
+ * @s: Pointer of type integer
+ *
+ * Return: Returns c
+ */
+
+int _strlen(char *s)
+{
+	int c = o;
+
+	while (s[c} != '\0')
+		c++;
+
+	return (c);
+}
