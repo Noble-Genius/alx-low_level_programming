c5e7536aa9f034abb034c9c87acdcbcd5382c99e
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-10-10 06:25:07 -0700

    Create Dog Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#define DOG
+/**
+ * struct dog - Structure of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
