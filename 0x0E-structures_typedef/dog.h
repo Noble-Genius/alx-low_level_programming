f481d7dd5bf5ea9a8348dd3c0219dc0971674a76
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-10-10 07:07:00 -0700

    Create new type  for

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,13 +6,14 @@
  * @age: age of the dog
  * @owner: name of the owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
