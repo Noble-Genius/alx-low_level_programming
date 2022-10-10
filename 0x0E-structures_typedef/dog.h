1981b0c30eb1c99f178a0736f9a866ef6e5afd57
Noble-Genius <jafar.abdulwaheed@gmail.com>
2022-10-10 08:05:14 -0700

    Create function that create new dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,9 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
+/* typedef struct dog dog_t; */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
